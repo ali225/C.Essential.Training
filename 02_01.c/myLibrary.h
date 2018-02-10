be648a1d86307a953b4f44c6119900ea43006670
ali225 <aligamal225@gmail.com>
2018-02-10 22:43:29 +0200

    to day write cod c program

---
+++ 02_01.c/myLibrary.h
@@ -0,0 +1,14 @@
+/*
+ * myLibrary.c
+ *
+ *  Created on: 
+ *      Author: 
+ */
+void function1(void){
+	puts("It works :)");
+}
+
+void function2(void){
+	//This function does nothing as well
+}
+
---
+++ 02_02.c/specialTasks.h
@@ -0,0 +1,17 @@
+/*
+ * specialTasks.h
+ *
+ *  Created on: February 10, 2018
+ *      Author: Eng Ali GamaL Aziz 
+ */
+
+#ifndef SPECIALTASKS_H_
+#define SPECIALTASKS_H_
+
+void specialTask_1(void);
+void specialTask_2(void);
+void specialTask_3(void);
+void specialTask_4(void);
+void specialTask_5(void);
+
+#endif /* SPECIALTASKS_H_ */
